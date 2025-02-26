ec971b3fb870bad2596f368f9b4df8b022b0ac40
Andra Putra <ricardoandra17@gmail.com>
2025-02-26 21:20:18 +0700

    fix logo

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -1,6 +1,7 @@
 // import { useRef } from 'react';
 // import { Player } from '@lordicon/react';
 import { AspectRatio, Center, Image } from '@mantine/core';
+import logo from '@/assets/icon/bpkp.png'
 // import ICON from '@/assets/icon/flat-book.json';
 
 export function Logo() {
@@ -9,7 +10,7 @@ export function Logo() {
   return (
     <AspectRatio ratio={50 / 50} maw={50} ml={20} mt={5} my="auto">
       <Center>
-        <Image src="/src/assets/icon/bpkp.png" alt="Logo" />
+        <Image src={logo} alt="Logo" />
         {/* <Player ref={playerRef} icon={ICON} size={50} /> */}
       </Center>
     </AspectRatio>
